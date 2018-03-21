14ede6e5b4a94a61200d0b803f43b79cea7ca110
vijay013 <vijayn013@gmail.com>
2018-03-21 10:55:11 +0530

    Test case created

---
+++ src/main/java/org/demogit/Testgit.java
@@ -0,0 +1,8 @@
+package org.demogit;
+
+public class Testgit {
+	public static void main(String[] args) {
+		System.out.println("test case1");
+	}
+
+}
---
+++ src/main/java/org/selmaven/Gitdemo/App.java
@@ -0,0 +1,13 @@
+package org.selmaven.Gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
