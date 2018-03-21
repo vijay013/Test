e9bdb6b9c93e22dc06a88e9ec9990b17a7b266a3
vijay013 <vijayn013@gmail.com>
2018-03-21 12:31:10 +0530

    Test altered

--- src/main/java/org/demogit/Testgit.java
+++ src/main/java/org/demogit/Testgit.java
@@ -4,6 +4,7 @@ public class Testgit {
 	public static void main(String[] args) {
 		System.out.println("test case1");
 		System.out.println("test case2");
+		System.out.println("test case3");
 	}
 
 }
